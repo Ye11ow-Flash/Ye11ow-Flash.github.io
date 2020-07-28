2c3f014445cbc91923550abeab321b9c4aa863aa
Ye11ow-Flash <jaineel.ns@somaiya.edu>
2020-07-28 13:05:07 +0530

    alignment of aboutus

--- script.js
+++ script.js
@@ -84,60 +84,60 @@ function setTheme(mode){
 
 	}
 
-	if(mode == 'purple'){
-		var var1 = document.getElementById('hero-area');
-		var var2 = document.getElementById('about');
-		var var3 = document.getElementById('services');
-		var var4 = document.getElementById('resume');
-		var var5 = document.getElementById('position');
-		var var6 = document.getElementById('publications');
-		var var7 = document.getElementById('portfolios');
-		var var8 = document.getElementById('courses');
-		var var9 = document.getElementById('achievements');
-		var var10 = document.getElementById('sketch');
-		var var11 = document.getElementById('contact');
-		var var12 = document.getElementById('foot');
+	// if(mode == 'purple'){
+	// 	var var1 = document.getElementById('hero-area');
+	// 	var var2 = document.getElementById('about');
+	// 	var var3 = document.getElementById('services');
+	// 	var var4 = document.getElementById('resume');
+	// 	var var5 = document.getElementById('position');
+	// 	var var6 = document.getElementById('publications');
+	// 	var var7 = document.getElementById('portfolios');
+	// 	var var8 = document.getElementById('courses');
+	// 	var var9 = document.getElementById('achievements');
+	// 	var var10 = document.getElementById('sketch');
+	// 	var var11 = document.getElementById('contact');
+	// 	var var12 = document.getElementById('foot');
 
-		var bg1 = "#46344E";
-		var bg2 = "#9774a3";
-		var foot = "#1e2c22";
-		var2.style.background = bg1;
-		var3.style.background = bg2;
-		var4.style.background = bg1;
-		var5.style.background = bg2;
-		var6.style.background = bg1;
-		var7.style.background = bg2;
-		var8.style.background = bg1;
-		var9.style.background = bg2;
-		var11.style.background = bg2;
-		var12.style.background = foot;
+	// 	var bg1 = "#46344E";
+	// 	var bg2 = "#9774a3";
+	// 	var foot = "#1e2c22";
+	// 	var2.style.background = bg1;
+	// 	var3.style.background = bg2;
+	// 	var4.style.background = bg1;
+	// 	var5.style.background = bg2;
+	// 	var6.style.background = bg1;
+	// 	var7.style.background = bg2;
+	// 	var8.style.background = bg1;
+	// 	var9.style.background = bg2;
+	// 	var11.style.background = bg2;
+	// 	var12.style.background = foot;
 
-		var var13 = document.getElementById('filter1');
-		var var14 = document.getElementById('filter2');
-		var var15 = document.getElementById('filter3');
-		var var16 = document.getElementById('filter4');
-		var var17 = document.getElementById('filter5');
-		var var18 = document.getElementById('filter0');
+	// 	var var13 = document.getElementById('filter1');
+	// 	var var14 = document.getElementById('filter2');
+	// 	var var15 = document.getElementById('filter3');
+	// 	var var16 = document.getElementById('filter4');
+	// 	var var17 = document.getElementById('filter5');
+	// 	var var18 = document.getElementById('filter0');
 
-		var bg_btn = "black";
-		var13.style.background = bg_btn;
-		var14.style.background = bg_btn;
-		var15.style.background = bg_btn;
-		var16.style.background = bg_btn;
-		var17.style.background = bg_btn;
-		var18.style.background = bg_btn;
+	// 	var bg_btn = "black";
+	// 	var13.style.background = bg_btn;
+	// 	var14.style.background = bg_btn;
+	// 	var15.style.background = bg_btn;
+	// 	var16.style.background = bg_btn;
+	// 	var17.style.background = bg_btn;
+	// 	var18.style.background = bg_btn;
 		
-		var var19 = document.getElementsByClassName('portfolio_style');
-		for(var i = 0; i < var19.length; i++){
-			var19[i].style.borderColor = bg_btn;
-		}
+	// 	var var19 = document.getElementsByClassName('portfolio_style');
+	// 	for(var i = 0; i < var19.length; i++){
+	// 		var19[i].style.borderColor = bg_btn;
+	// 	}
 
-		var text = "white";
-		var var20 = document.getElementsByClassName('text');
-		for(var i = 0; i < var20.length; i++){
-			var20[i].style.color = text;
-		}
-	}
+	// 	var text = "white";
+	// 	var var20 = document.getElementsByClassName('text');
+	// 	for(var i = 0; i < var20.length; i++){
+	// 		var20[i].style.color = text;
+	// 	}
+	// }
 
 	if(mode == 'blue'){
 		var var1 = document.getElementById('hero-area');
@@ -247,6 +247,12 @@ function setTheme(mode){
 		for(var i = 0; i < var20.length; i++){
 			var20[i].style.color = text;
 		}
+
+		// var var21 = document.getElementsByClassName('proj_grouping_btn');
+		// var btn_grad = "linear-gradient(90deg, #606B01, #8D846C) !important";
+		// for(var i = 0; i < var21.length; i++){
+		// 	var21[i].style.background = btn_grad;
+		// }
 
 	}
 
